8b10c4bffe4ebc5e3d979a3a07569ed15716135d
Mohammed Elamine <51944814+mohammed-esi@users.noreply.github.com>
2026-01-27 18:02:28 +0100

    feat: update the dashboard links with admin and member users

--- src/constant/layout.ts
+++ src/constant/layout.ts
@@ -10,3 +10,21 @@ export const navItems = [
   },
   { label: 'Pricing', href: '#' },
 ] as const;
+
+
+export type NavItem = {
+  label: string;
+  href: string;
+  icon: string;
+  role?: string
+};
+
+export const sideNav: NavItem[] = [
+  { label: 'Home', href: '/dashboard', icon: 'solar:home-2-linear' },
+  { label: 'Profile', href: '/dashboard/profile', icon: 'ion:person-outline' },
+  { label: 'Tokens', href: '/dashboard/tokens', icon: 'material-symbols:token-outline-rounded' },
+  { label: 'Support', href: '/dashboard/support', icon: 'ix:support' },
+  { label: 'Settings', href: '/dashboard/settings', icon: 'ci:settings' },
+  { label: 'Doc Editor', href: '/dashboard/docs', icon: 'f7:doc', role: 'admin' },
+  { label: 'Admin Tool', href: '/dashboard/tool', icon: 'flowbite:tools-outline', role: 'admin' },
+];
